f864d84b222c680b9753b4c1d8f8c1a2302e1444
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-08-27 04:35:34 +0800

    eh

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 # code ref: https://levelup.gitconnected.com/create-your-own-chatbox-and-train-it-in-less-than-30-lines-of-code-in-python-using-openai-and-34b1e9bc5c30
+# github: https://github.com/ebojacky/console-chatbox-using-openai-and-langchain
 
 import os
 import openai
@@ -27,9 +28,9 @@ else:
   else:
     index = VectorstoreIndexCreator().from_loaders([loader])
 
-embeddings = OpenAIEmbeddings(openai_api_key="OPEN_API_KEY")
+embeddings = OpenAIEmbeddings(openai_api_key=constants.APIKEY)
 vectors = FAISS.from_documents(loader, embeddings)
-chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(temperature=0.0, model_name='text-davinci-003', openai_api_key=api_key), retriever=vectors.as_retriever())
+chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(temperature=0.0, model_name='text-davinci-003', openai_api_key=constants.APIKEY), retriever=vectors.as_retriever())
 history = []
 
 while True:
