ab288b7cb57e21d19f856a81b7b95f80cdb21284
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-08-27 11:13:02 +0800

    half fixed

--- app.py
+++ app.py
@@ -1,16 +1,56 @@
 import os
 import constants 
-
+import sys
 import streamlit as st
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.llms import OpenAI
+from langchain.vectorstores import Chroma
+
+import constants
 
 os.environ['OPENAI_API_KEY'] = constants.APIKEY
 
 st.title('SMU Libraries GPT')
-prompt = st.text_input('Prompt away!')
+prompt = st.text_input('Ask me anything about SMU Libraries!')
 
-llm = OpenAI(temperature=0.4)
+# llm = OpenAI(temperature=0.4)
 
-if prompt:
-    response = llm(prompt)
-    st.write(response)
+# if prompt:
+#     response = llm(prompt)
+#     st.write(response)
+
+PERSIST = False
+
+if len(sys.argv) > 1:
+  prompt = sys.argv[1]
+
+if PERSIST and os.path.exists("persist"):
+  print("Reusing index...\n")
+  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+else:
+  loader = TextLoader("data.txt")
+  #loader = DirectoryLoader("data/")
+  if PERSIST:
+    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+  else:
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+chain = ConversationalRetrievalChain.from_llm(
+  llm=ChatOpenAI(model="gpt-3.5-turbo"),
+  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+)
+
+chat_history = []
+
+while True:
+    result = chain({"question": prompt, "chat_history": chat_history})
+    st.write(result['answer'])
+    break
+
+chat_history.append((prompt, result['answer']))
